a393427a2a0cfecdc18c00ed7dd36b64495d2088
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-01-11 21:50:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
 import { question } from 'readline-sync'
 
-let answer = question('What is your favorite color?')
-console.log(answer)
+// let answer = question('Who are you?')
+// console.log(`Hello ${answer} nice to meet you`)
 
-let x = Number(question('What is your favorite number?'))
-console.log(x)
+
+//Uppgift 1
+
+// let city= question('Where are you from?')
+// if( city== 'Karlstad'){
+  //   console.log(`What do you think of ${city} ${answer}?`)
+  // } else{
+    //   console.log(`Oh thats a nice city, why dont you move to Karlstad?`)
+    // }
+    
+//Uppgift 2
+// let x= Number(question('Please enter a number '))
+// let y= Number(question('another one '))
+// console.log(`The sum is ${x + y}`)
+
+
+//Uppgift 3
+// let fråga= question('Write a password ')
+// const lösenord=('Ali17ali17')
+
+// if (fråga== lösenord){
+  //   console.log('Well done you took the same password as mine ')
+  
+  // }else{
+    //   console.log(`Wrong password, the right password should be ${lösenord}`)
+    // }
+    
+    
+//Uppgift 4
+// let x= Number(question('Skriv ett tal: '));
+
+// if (x < 100) {
+//   console.log('Talet är mindre än 100');
+// }else if (x == 100){
+//   console.log('Talet är lika med 100');
+// }else if (x > 100) {
+//   console.log('Talet är större än 100')
+// }
+
+let guess=Number(question('Guess the number: '))
+let secret = Math.floor(Math.random() * 100) + 1
+let guessCount = 0;
+let maxGuesses = 10;
+
+if (guess == secret) {
+  console.log('Amazing your answer were correct')
+} else if (guess > secret ) {
+  console.log('Your number were to high')
+} else if (guess < secret) {
+  console.log('Your number were to low')
+}
+
